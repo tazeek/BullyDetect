059b56d21c9aca8a707e53afc8b5007cb5bd1d09
Tazeek <tazeek.rakib@gmail.com>
2016-11-04 02:01:43 +0800

    Unigram model trained. Now training for bigram

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ window = 5 # Window of surrounding words
 alpha = 0.025 # Initial learning rate of the Neural Network
 min_count = 5 # Minimum Frequency of Words
 workers = multiprocessing.cpu_count() # Number of workers
-max_vocab_size = 6000000 # Number of Unique Words
+max_vocab_size = 8000000 # Number of Unique Words
 negative = 10 # Number of words to be drawn for Negative Sampling
 sample = 0.001 # Subsampling of frequent words 
 hs = 0 # Negative Sampling to be used
@@ -56,13 +56,13 @@ iter = 5 # Iterations over corpus. Also called epochs
 # (Number of Unique Words x Dimension Size x 12)/1,000,000,000 
 
 #Initialize Bigram Transformer
-#bigram_transformer = Phrases(word_list)
+bigram_transformer = Phrases(word_list)
 
 os.system('cls')
 
 #Initialize Word2Vec model 
-model = Word2Vec(word_list, sg=sg, size=size, window=window, alpha=alpha, min_count=min_count, workers=workers, max_vocab_size=max_vocab_size, hs=hs, iter=iter, sample=sample)
-model_name = "reddit_bigram"
+model = Word2Vec(bigram_transformer[word_list], sg=sg, size=size, window=window, alpha=alpha, min_count=min_count, workers=workers, max_vocab_size=max_vocab_size, hs=hs, iter=iter, sample=sample)
+model_name = "reddit_full_bi"
 
 model.init_sims(replace=True) # Trim down memory size
 model.save_word2vec_format(model_name + '.bin', binary=True) 
