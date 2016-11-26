47d24b8e68bc0383fcca1837a6c9abf335f4507c
Tazeek <tazeek.rakib@gmail.com>
2016-11-26 23:54:27 +0800

    Storage complete. Now re-training

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ os.system('cls')
 
 #Initialize Word2Vec model 
 model = Word2Vec(word_list, sg=sg, size=size, window=window, alpha=alpha, min_count=min_count, workers=workers, max_vocab_size=max_vocab_size, hs=hs, iter=iter, sample=sample)
-model_name = "reddit_full_UNI"
+model_name = "w2v_reddit_unigram_300d"
 
 model.init_sims(replace=True) # Trim down memory size
 model.save_word2vec_format(model_name + '.bin', binary=True) 
