cdf6d753537cdb22bcb2dfa4cbd6265deb560ad3
tazeek <tazeek.rakib@gmail.com>
2016-12-06 22:50:37 +0800

    Training Successful

--- tfidf_train.py
+++ tfidf_train.py
@@ -4,17 +4,32 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the dataset
+print("LOADING DATASET\n\n")
 df = pd.read_csv('clean_dataset.csv')
 
 # Load the comments
+print("LOADING COMMENTS\n\n")
 X = df['Comment']
 
 # Initialize the model: One with stop words, One without
+print("INITIALIZING TFIDF WITH STOP WORDS\n\n")
 tfidf_stop_words = TfidfVectorizer()
+
+print("INITIALIZING TFIDF WITHOUT STOP WORDS\n\n")
 tfidf = TfidfVectorizer(stop_words='english')
 
 # Train both
+print("TRAINING TFIDF WITH STOP WORDS\n\n")
 tfidf_stop_words.fit(X)
+
+print("TRAINING TFIDF WITHOUT STOP WORDS\n\n")
 tfidf.fit(X)
 
 # Save both models
+print("SAVING TFIDF WITH STOP WORDS\n\n")
+FILE = "TFIDF models/tfidif_stop.pk"
+pickle.dump(tfidf_stop_words, open(FILE, "wb"))
+
+print("SAVING TFIDIF WITHOUT STOP WORDS\n\n")
+FILE = "TFIDF models/tfidf_normal.pk"
+pickle.dump(tfidf, open(FILE, "wb"))
