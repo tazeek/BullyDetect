62ccba8f1cb036c364119763acc35d6e06bb674a
tazeek <tazeek.rakib@gmail.com>
2016-12-07 19:51:53 +0800

    Word feature using hybrid approach and 10 fold cross validation

--- word_avg_hybrid.py
+++ word_avg_hybrid.py
@@ -9,51 +9,76 @@ from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from gensim.models import Word2Vec as w2v
 
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, KFold
 from sklearn.metrics import accuracy_score, confusion_matrix 
 from sklearn.model_selection import cross_val_score
 
 # The testing function
-def testing(model, model_name, X_train, y_train, X_test, y_test):
+def testing(model, model_name, X, y, cv):
 
 	print(model_name + " STARTS HERE\n\n")
 
-	# Train the model
-	model.fit(X_train , y_train)
+	# Array to store results
+	accuracy_array = []
+	precision_array = []
+	fdr_array = []
+	fpr_array = []
+	execution_time_array = []
 
-	# Predict and calculate run-time
-	start = time.time()
-	result = model.predict(X_test)
-	end = time.time()
+	for train_cv, test_cv in cv:
 
-	# Confusion Matrix
-	cm = confusion_matrix(y_true=y_test, y_pred=result)
+		# Seperate the training and testing fold
+		X_train, X_test = X[train_cv], X[test_cv]
+		y_train, y_test = y[train_cv], y[test_cv]
 
-	tp = cm[1][1] # True positives
-	fp = cm[0][1] # False positives
-	tn = cm[0][0] # True negatives
-	fn = cm[1][0] # False negatives
+		# Train the model
+		model.fit(X_train , y_train)
 
-	# Evaluation Metrics
-	accuracy = accuracy_score(y_true=y_test , y_pred=result)
-	precision = tp/(tp+fp)
-	fdr = 1 - precision # False Discovery Rate
-	fpr = fp/(fp + tn) # False Positive Rate
+		# Predict and calculate run-time
+		start = time.time()
+		result = model.predict(X_test)
+		end = time.time()
 
-	#precision = precision_score(y_true=y_test, y_pred=result)
+		execution_time = end - start
+
+		# Confusion Matrix
+		cm = confusion_matrix(y_true=y_test, y_pred=result)
+
+		tp = cm[1][1] # True positives
+		fp = cm[0][1] # False positives
+		tn = cm[0][0] # True negatives
+		fn = cm[1][0] # False negatives
+
+		# Evaluation Metrics
+		accuracy = accuracy_score(y_true=y_test , y_pred=result)
+		precision = tp/(tp+fp)
+		fdr = 1 - precision # False Discovery Rate
+		fpr = fp/(fp + tn) # False Positive Rate
+
+		# Append results
+		accuracy_array.append(accuracy)
+		precision_array.append(precision)
+		fdr_array.append(fdr)
+		fpr_array.append(fpr)
+		execution_time_array.append(execution_time)
+
+	# Get mean results
+	mean_accuracy = np.mean(accuracy_array)
+	mean_precision = np.mean(precision_array)
+	mean_fdr = np.mean(fdr_array)
+	mean_fpr = np.mean(fpr_array)
+	mean_execution_time = np.mean(execution_time_array)
 
 	# Display results
-	print("ACCURACY: ", round(accuracy*100, 2))
-	print("PRECISION: ", round(precision*100, 2))
-	print("FALSE DISCOVERY RATE: ", round(fdr*100, 2))
-	print("FALSE POSITIVE RATE: ", round(fpr*100, 2), "\n")
-	print("TRUE POSITIVES: ", tp)
-	print("FALSE POSITIVES:",fp,"\n")
-	print("TRUE NEGATIVES: ", tn)
-	print("FALSE NEGATIVES: ", fn,"\n")
-	print("RUN TIME: ", end - start)
-
-	print("\n\n" + model_name + " STOPS HERE\n\n")
+	print("MEAN ACCURACY: ", round(mean_accuracy*100, 2))
+	print("MEAN PRECISION: ", round(mean_precision*100, 2), "\n")
+	print("MEAN FALSE DISCOVERY RATE: ", round(mean_fdr*100, 2))
+	print("MEAN FALSE POSITIVE RATE: ", round(mean_fpr*100, 2), "\n")
+	#print("TRUE POSITIVES: ", tp)
+	#print("FALSE POSITIVES:",fp,"\n")
+	#print("TRUE NEGATIVES: ", tn)
+	#print("FALSE NEGATIVES: ", fn,"\n")
+	print("MEAN RUN TIME: ", mean_execution_time)
 
 
 # Use each word as a feature
@@ -116,35 +141,41 @@ df = pd.read_csv('clean_dataset.csv')
 X , y = df['Comment'], df['Insult']
 split = 3900
 
-# Split the sample or make your own sample
-print("SPLITTING DATA\n\n")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=0)
-#X_train, y_train = df['Comment'][:split], df['Insult'][:split]
-#X_test, y_test = df['Comment'][split:], df['Insult'][split:]
-
 # Load the dictionary
 print("LOADING DICTIONARY\n\n")
 FILE = "Word Dictionaries/vect_dict_5.p"
 vect_dict = pickle.load(open(FILE,"rb"))
 
-# Data Transformation (4th parameter indicates maximum words allowed)
+# Transform the data
 MAX_WORDS = 500
+X = commentFeatureVecs(X, model, vect_dict, MAX_WORDS)
 
-print("TRANSFORMING TRAINING SET\n\n")
-X_train = commentFeatureVecs(X_train, model, vect_dict, MAX_WORDS)
+# Split the sample or make your own sample
+#print("SPLITTING DATA\n\n")
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=0)
+#X_train, y_train = df['Comment'][:split], df['Insult'][:split]
+#X_test, y_test = df['Comment'][split:], df['Insult'][split:]
 
-print("TRANSFORMING TESTING SET\n\n")
-X_test = commentFeatureVecs(X_test , model, vect_dict, MAX_WORDS)
+#print("TRANSFORMING TRAINING SET\n\n")
+#X_train = commentFeatureVecs(X_train, model, vect_dict, MAX_WORDS)
+
+#print("TRANSFORMING TESTING SET\n\n")
+#X_test = commentFeatureVecs(X_test , model, vect_dict, MAX_WORDS)
 
 # Implement Classifier(s) here and store in dictionary
+print("INITIALIZING CLASSIFIERS \n\n")
 nb = GaussianNB()
 rf = RandomForestClassifier(n_estimators=100)
 svm = LinearSVC()
 
 models = { "Naive Bayes": nb, "Support Vector Machines": svm, "Random Forest": rf}
 
+# Test with 10 fold cross validation
+print("TESTING WITH 10 FOLD CROSS VALIDATION\n\n")
+cv = KFold(n=len(X), shuffle=False, n_folds=10)
+
 print("TESTING  MODELS\n\n")
 for key, value in models.items():
 	
 	# Test each model
-	testing(value, key, X_train, y_train, X_test, y_test)
+	testing(value, key, X, y, cv)
