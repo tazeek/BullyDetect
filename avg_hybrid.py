3c87b3a282591591959f7da0c489f6b0b847508f
tazeek <tazeek.rakib@gmail.com>
2016-12-10 01:45:02 +0800

    Three more files revamped completely

--- avg_hybrid.py
+++ avg_hybrid.py
@@ -1,84 +1,16 @@
 import pandas as pd 
 import numpy as np 
-import time
 import os
 import pickle
 
-from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from gensim.models import Word2Vec as w2v
 
-from sklearn.cross_validation import train_test_split, KFold
-from sklearn.metrics import accuracy_score, confusion_matrix 
+from sklearn.model_selection import StratifiedKFold
 
-def testing(model, model_name, X, y, cv):
-
-	print(model_name + " STARTS HERE\n\n")
-
-	# Array to store results
-	accuracy_array = []
-	precision_array = []
-	fdr_array = []
-	fpr_array = []
-	execution_time_array = []
-
-	for train_cv, test_cv in cv:
-
-		# Seperate the training and testing fold
-		X_train, X_test = X[train_cv], X[test_cv]
-		y_train, y_test = y[train_cv], y[test_cv]
-
-		# Train the model
-		model.fit(X_train , y_train)
-
-		# Predict and calculate run-time
-		start = time.time()
-		result = model.predict(X_test)
-		end = time.time()
-
-		execution_time = end - start
-
-		# Confusion Matrix
-		cm = confusion_matrix(y_true=y_test, y_pred=result)
-
-		tp = cm[1][1] # True positives
-		fp = cm[0][1] # False positives
-		tn = cm[0][0] # True negatives
-		fn = cm[1][0] # False negatives
-
-		# Evaluation Metrics
-		accuracy = accuracy_score(y_true=y_test , y_pred=result)
-		precision = tp/(tp+fp)
-		fdr = 1 - precision # False Discovery Rate
-		fpr = fp/(fp + tn) # False Positive Rate
-
-		# Append results
-		accuracy_array.append(accuracy)
-		precision_array.append(precision)
-		fdr_array.append(fdr)
-		fpr_array.append(fpr)
-		execution_time_array.append(execution_time)
-
-	# Get mean results
-	mean_accuracy = np.mean(accuracy_array)
-	mean_precision = np.mean(precision_array)
-	mean_fdr = np.mean(fdr_array)
-	mean_fpr = np.mean(fpr_array)
-	mean_execution_time = np.mean(execution_time_array)
-
-	# Display results
-	print("MEAN ACCURACY: ", round(mean_accuracy*100, 2))
-	print("MEAN PRECISION: ", round(mean_precision*100, 2), "\n")
-	print("MEAN FALSE DISCOVERY RATE: ", round(mean_fdr*100, 2))
-	print("MEAN FALSE POSITIVE RATE: ", round(mean_fpr*100, 2), "\n")
-	#print("TRUE POSITIVES: ", tp)
-	#print("FALSE POSITIVES:",fp,"\n")
-	#print("TRUE NEGATIVES: ", tn)
-	#print("FALSE NEGATIVES: ", fn,"\n")
-	print("MEAN RUN TIME: ", mean_execution_time)
-
-	print("\n\n" + model_name + " STOPS HERE\n\n")
+from evaluation import evaluatingModel 
 
 # One of the kaggle tests 
 def makeFeatureVec(words, model, vector_dict, num_features):
@@ -95,9 +27,6 @@ def makeFeatureVec(words, model, vector_dict, num_features):
 
 		if word in model: #and word not in stop_words:
 			nwords += 1.
-
-			# Get average of the word
-			#avgWordFeature = wordsAverage(word, model)
 
 			# Add to the vector
 			featureVec = np.add(featureVec, vector_dict[word])
@@ -139,7 +68,6 @@ df = pd.read_csv('clean_dataset.csv')
 
 # Separate out comments and labels
 X , y = df['Comment'], df['Insult']
-split = 3900
 
 # Load the dictionary
 print("LOADING DICTIONARY\n\n")
@@ -150,34 +78,18 @@ vector_dict = pickle.load(open(FILE,"rb"))
 print("TRANSFORMING DATA \n\n")
 X = getAvgFeatureVecs(X, model, vector_dict, 300)
 
-# Split the sample or make your own sample
-#print("SPLITTING DATA\n\n")
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-#X_train, y_train = df['Comment'][:split], df['Insult'][:split]
-#X_test, y_test = df['Comment'][split:], df['Insult'][split:]
-
-# Data Transformation
-#print("TRANSFORMING TRAINING SET\n\n")
-#X_train = getAvgFeatureVecs(X_train, model, vector_dict, 300)
-
-#print("TRANSFORMING TESTING SET\n\n")
-#X_test = getAvgFeatureVecs(X_test , model, vector_dict, 300)
-
-#X = getAvgFeatureVecs(X, model, vector_dict, 300)
-
 # Implement Classifier(s) here and store in dictionary
-print("INITIALIZING CLASSIFIERS \n\n")
+print("INITLIAZING CLASSIFIERS \n\n")
 nb = GaussianNB()
 rf = RandomForestClassifier(n_estimators=100)
 svm = LinearSVC()
 
-models = { "Naive Bayes": nb, "Support Vector Machines": svm, "Random Forest": rf}
+# Store them in a dicitonary
+models = { "NB": nb, "SVM": svm, "RF": rf}
 
-# Testing with 10 fold cross validation
-print("TESTING WITH 10 FOLD CROSS VALIDATION\n\n")
-cv = KFold(n=len(X), shuffle=False, n_folds=10)
+
+# Test with 10 fold Cross validation/Stratified K Fold
+skf = StratifiedKFold(n_splits=10)
 
 for key, value in models.items():
-	
-	# Test each model
-	testing(value, key, X, y, cv)
+	evaluatingModel(value, key, X, y, skf)
--- avg_hybrid_tfidf.py
+++ avg_hybrid_tfidf.py
@@ -1,86 +1,18 @@
 import pandas as pd 
 import numpy as np 
-import time
 import os
 import pickle
 
 from collections import defaultdict
 
-from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from gensim.models import Word2Vec as w2v
 
-from sklearn.cross_validation import train_test_split, KFold
-from sklearn.metrics import accuracy_score, confusion_matrix 
+from sklearn.model_selection import StratifiedKFold
 
-# The testing function
-def testing(model, model_name, X, y, cv):
-
-	print(model_name + " STARTS HERE\n\n")
-
-	# Array to store results
-	accuracy_array = []
-	precision_array = []
-	fdr_array = []
-	fpr_array = []
-	execution_time_array = []
-
-	for train_cv, test_cv in cv:
-
-		# Seperate the training and testing fold
-		X_train, X_test = X[train_cv], X[test_cv]
-		y_train, y_test = y[train_cv], y[test_cv]
-
-		# Train the model
-		model.fit(X_train , y_train)
-
-		# Predict and calculate run-time
-		start = time.time()
-		result = model.predict(X_test)
-		end = time.time()
-
-		execution_time = end - start
-
-		# Confusion Matrix
-		cm = confusion_matrix(y_true=y_test, y_pred=result)
-
-		tp = cm[1][1] # True positives
-		fp = cm[0][1] # False positives
-		tn = cm[0][0] # True negatives
-		fn = cm[1][0] # False negatives
-
-		# Evaluation Metrics
-		accuracy = accuracy_score(y_true=y_test , y_pred=result)
-		precision = tp/(tp+fp)
-		fdr = 1 - precision # False Discovery Rate
-		fpr = fp/(fp + tn) # False Positive Rate
-
-		# Append results
-		accuracy_array.append(accuracy)
-		precision_array.append(precision)
-		fdr_array.append(fdr)
-		fpr_array.append(fpr)
-		execution_time_array.append(execution_time)
-
-	# Get mean results
-	mean_accuracy = np.mean(accuracy_array)
-	mean_precision = np.mean(precision_array)
-	mean_fdr = np.mean(fdr_array)
-	mean_fpr = np.mean(fpr_array)
-	mean_execution_time = np.mean(execution_time_array)
-
-	# Display results
-	print("MEAN ACCURACY: ", round(mean_accuracy*100, 2))
-	print("MEAN PRECISION: ", round(mean_precision*100, 2), "\n")
-	print("MEAN FALSE DISCOVERY RATE: ", round(mean_fdr*100, 2))
-	print("MEAN FALSE POSITIVE RATE: ", round(mean_fpr*100, 2), "\n")
-	#print("TRUE POSITIVES: ", tp)
-	#print("FALSE POSITIVES:",fp,"\n")
-	#print("TRUE NEGATIVES: ", tn)
-	#print("FALSE NEGATIVES: ", fn,"\n")
-	print("MEAN RUN TIME: ", mean_execution_time)
-
+from evaluation import evaluatingModel 
 
 # One of the kaggle tests 
 def makeFeatureVec(words, model, vector_dict, tfidf_model, num_features):
@@ -97,9 +29,6 @@ def makeFeatureVec(words, model, vector_dict, tfidf_model, num_features):
 
 		if word in model: #and word not in stop_words:
 			nwords += 1.
-
-			# Get average of the word
-			#avgWordFeature = wordsAverage(word, model)
 
 			# Add to the vector
 			avgWordsFeature = np.multiply(vector_dict[word], tfidf_model[word])
@@ -165,7 +94,6 @@ df = pd.read_csv('clean_dataset.csv')
 
 # Separate out comments and labels
 X , y = df['Comment'], df['Insult']
-split = 3900
 
 # Load the dictionary
 print("LOADING INNOVATIVE DICTIONARY\n\n")
@@ -176,32 +104,18 @@ vector_dict = pickle.load(open(FILE,"rb"))
 print("TRANSFORMING DATA \n\n")
 X = getAvgFeatureVecs(X, model, vector_dict, tfidf_model, 300)
 
-#print("TRANSFORMING TRAINING SET\n\n")
-#X_train = getAvgFeatureVecs(X_train, model, vector_dict, tfidf_model, 300)
-
-#print("TRANSFORMING TESTING SET\n\n")
-#X_test = getAvgFeatureVecs(X_test , model, vector_dict, tfidf_model, 300)
-
-# Split the sample or make your own sample
-#print("SPLITTING DATA\n\n")
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-#X_train, y_train = df['Comment'][:split], df['Insult'][:split]
-#X_test, y_test = df['Comment'][split:], df['Insult'][split:]
-
 # Implement Classifier(s) here and store in dictionary
-print("INITIALIZING CLASSIFIERS \n\n")
+print("INITLIAZING CLASSIFIERS \n\n")
 nb = GaussianNB()
 rf = RandomForestClassifier(n_estimators=100)
 svm = LinearSVC()
 
-models = { "Naive Bayes": nb, "Support Vector Machines": svm, "Random Forest": rf}
+# Store them in a dicitonary
+models = { "NB": nb, "SVM": svm, "RF": rf}
 
-# Test with 10 fold cross validation
-print("TESTING WITH 10 FOLD CROSS VALIDATION\n\n")
-cv = KFold(n=len(X), shuffle=False, n_folds=10)
 
-print("TESTING  MODELS\n\n")
+# Test with 10 fold Cross validation/Stratified K Fold
+skf = StratifiedKFold(n_splits=10)
+
 for key, value in models.items():
-	
-	# Test each model
-	testing(value, key, X, y, cv)
+	evaluatingModel(value, key, X, y, skf)
