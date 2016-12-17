34f6e3607094f5c667ab22b19fd195e7c3750e89
tazeek <tazeek.rakib@gmail.com>
2016-12-17 19:26:20 +0800

    File created to convert word clusters to cluster dictionary

---
+++ dict_cluster.py
@@ -0,0 +1,39 @@
+import pickle
+import os
+
+# Specify the file
+FILE = "K-Means Models/full_250C.pk"
+
+# Load using pickle
+word_centroid_map =  pickle.load(open(FILE,"rb"))
+
+# Get the total number of clusters
+total_clusters = max(word_centroid_map.values()) + 1
+
+# Cluster all the words from various indexes as one. Then store them as dictionary
+# Dictionary contents: Cluster number, cluster contents
+array_dict = []
+    
+# Loop cluster by cluster
+for cluster_num in range(0, total_clusters):
+    
+    # Create a dictionary
+    cluster_dict = {}
+
+    # Get word list
+    word_list = [ word for word, cluster in word_centroid_map.items() if cluster == cluster_num ]
+    
+    # Store in dictionary 
+    cluster_dict["cluster_num"] = cluster_num
+    cluster_dict["word_list"] = word_list
+
+    # Append to array
+    array_dict.append(cluster_dict)
+
+    # Progress report for every 50 clusters
+    if len(array_dict) % 50 == 0:
+    	print("%i clusters stored" % (len(array_dict)))
+
+ # Save the array
+ FILE = "Word Dictionaries/dict_250C.pk"
+ pickle.dump(array_dict, open(FILE, "wb"))
