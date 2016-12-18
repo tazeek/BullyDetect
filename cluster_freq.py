222b1c155cee9af7f827fc06b76bf65e83008a88
tazeek <tazeek.rakib@gmail.com>
2016-12-18 22:16:49 +0800

    Transformation function added

--- cluster_freq.py
+++ cluster_freq.py
@@ -5,6 +5,27 @@ import pandas as pd
 import numpy as np 
 
 # Transform the data
+def createBagCentroids(comment):
+
+	# Pre-allocate the bag of centroids vector (for speed)
+	bag_of_centroids = np.zeros( total_clusters_500, dtype="float32" )
+
+	# Loop word by word
+	for word in SENTENCE.split():
+	    
+	    # Check if word is in dictionary
+	    if word in word_centroid_map_500:
+	        
+	        # Get index of the word
+	        index = word_centroid_map_500[word]
+	        
+	        # Print for evalution
+	        print(word, index)
+	        
+	        # Increment index of bag_of_centroids
+	        bag_of_centroids[index] += 1
+
+	return bag_of_centroids
 
 # Read in comment by comment
 
