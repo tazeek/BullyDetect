f033c7307d52d3ba1ca3841cc2b327aa0bc717d6
tazeek <tazeek.rakib@gmail.com>
2016-12-18 21:35:54 +0800

    Function added to load cluster dictionary

--- cluster_freq.py
+++ cluster_freq.py
@@ -3,9 +3,22 @@ import pickle
 import pandas as pd
 import numpy as np 
 
+# Function to load the cluster dictionary
+def loadClusterSet(FILE):
+
+	# File loaded here
+	word_centroid_map = pickle.load(open(FILE,"rb"))
+
+	return word_centroid_map
+
 # Load the dataset here
 print("LOADING DATASET \n\n")
 df = pd.read_csv('clean_dataset.csv')
 
 # Separate out comments and labels
 X , y = df['Comment'], df['Insult']
+
+# Loading the cluster dictionary here
+print("LOADING CLUSTER DICTIONARY \n\n")
+FILE = "K-Means Models/full_500C.pk"
+cluster_dictionary = loadClusterSet(FILE)
