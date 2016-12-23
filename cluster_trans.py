b59e7e24711c27bb1a6daadac08732bfd3108bfd
tazeek <tazeek.rakib@gmail.com>
2016-12-23 17:14:55 +0800

    Cluster model and W2V model loaded

---
+++ cluster_trans.py
@@ -0,0 +1,23 @@
+import pickle
+import os
+
+import numpy as np 
+
+from gensim.models import Word2Vec as w2v 
+
+os.system('cls')
+
+# Load the Cluster dictionary
+print("LOADING CLUSTER DICTIONARY \n\n")
+cluster_num = 500
+FILE = "Word Dictionaries/dict_" + str(cluster_num) + "C.pk"
+array_dict_cluster = pickle.load(open(FILE, "rb"))
+
+for cluster in array_dict_cluster:
+	print(cluster.keys())
+	break
+exit()
+# Load Word2Vec model 
+print("LOADING WORD2VEC MODEL \n\n")
+FILE = "W2V Models/w2v_reddit_unigram_300d.bin"
+model = w2v.load_word2vec_format(FILE, binary=True)
