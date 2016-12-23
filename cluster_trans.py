c27af7fb9fa423a6cc7c187bb4818c8882e5099b
tazeek <tazeek.rakib@gmail.com>
2016-12-23 17:17:49 +0800

    Creating function to get average

--- cluster_trans.py
+++ cluster_trans.py
@@ -7,6 +7,13 @@ from gensim.models import Word2Vec as w2v
 
 os.system('cls')
 
+def getAverageCluster(word_list, model):
+
+	for word in word_list:
+		print(word)
+
+	return
+
 # Load the Cluster dictionary
 print("LOADING CLUSTER DICTIONARY \n\n")
 cluster_num = 500
@@ -14,7 +21,9 @@ FILE = "Word Dictionaries/dict_" + str(cluster_num) + "C.pk"
 array_dict_cluster = pickle.load(open(FILE, "rb"))
 
 for cluster in array_dict_cluster:
-	print(cluster.keys())
+	words = cluster['word_list']
+	
+	getAverageCluster(words, "")
 	break
 exit()
 # Load Word2Vec model 
