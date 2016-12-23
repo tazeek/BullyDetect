190ad3cd7330cbfed5324418215a621a856f3656
tazeek <tazeek.rakib@gmail.com>
2016-12-23 17:23:14 +0800

    Transformation function complete

--- cluster_trans.py
+++ cluster_trans.py
@@ -9,10 +9,24 @@ os.system('cls')
 
 def getAverageCluster(word_list, model):
 
-	for word in word_list:
-		print(word)
+	# Get number of words
+	total_words = len(word_list)
 
-	return
+	# Pre-initialize an empty numpy array (for speed)
+	# 300 is used, as it is the number of vectors in Word2Vec
+	avgWordsFeature = np.zeros((300,),dtype="float32")
+
+	# Loop word by word
+	for word in word_list:
+		
+		# Add the word's feature vectors 
+		avgWordsFeature = np.add(avgWordsFeature, model[word])
+
+	
+	# Divide to get the mean
+	avgWordsFeature = np.divide(avgWordsFeature,total_words)
+
+	return avgWordsFeature
 
 # Load the Cluster dictionary
 print("LOADING CLUSTER DICTIONARY \n\n")
@@ -20,13 +34,20 @@ cluster_num = 500
 FILE = "Word Dictionaries/dict_" + str(cluster_num) + "C.pk"
 array_dict_cluster = pickle.load(open(FILE, "rb"))
 
-for cluster in array_dict_cluster:
-	words = cluster['word_list']
-	
-	getAverageCluster(words, "")
-	break
-exit()
 # Load Word2Vec model 
 print("LOADING WORD2VEC MODEL \n\n")
 FILE = "W2V Models/w2v_reddit_unigram_300d.bin"
 model = w2v.load_word2vec_format(FILE, binary=True)
+
+# Loop cluster by cluster
+print("STARTING TRANSFORMATIONS \n\n")
+for cluster in array_dict_cluster:
+
+	# Get the word list 
+	words = cluster['word_list']
+	
+	# Call the function
+	getAverageCluster(words, model)
+	break
+
+exit()
